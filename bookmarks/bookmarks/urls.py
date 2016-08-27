842cb601cc83be646a50beffac06ff16d4ae1808
Tom Henderson <tomhenderson@mac.com>
2016-08-28 11:59:11 +1200

    Added tag filters. Simplified template headers.

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -5,13 +5,14 @@ from django.contrib import admin
 from django.conf import settings
 
 import views
-from core.views import BookmarksList
+from core.views import BookmarksList, BookmarkTagList
 import django.contrib.auth.views
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', BookmarksList.as_view()),
+    url(r'^$', BookmarksList.as_view(), name='index'),
+    url(r'^tag/(?P<slug>[^/]+)/$', BookmarkTagList.as_view(), name='tag'),
     url(r'^login/$', django.contrib.auth.views.login, name='log_in'),
     url(r'^logout/$', views.log_out, name='log_out'),
     url(r'^admin/', include(admin.site.urls)),
--- bookmarks/core/views.py
+++ bookmarks/core/views.py
@@ -5,12 +5,22 @@ from .models import Bookmark
 
 
 class BookmarksList(ListView):
-    queryset = Bookmark.objects.all().order_by('-add_date')
-    # template_name = 'core/bookmarks.html'
+    model = Bookmark
+    ordering = '-date_added'
     context_object_name = 'bookmarks'
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
         context = super(BookmarksList, self).get_context_data(**kwargs)
         context['title'] = "Bookmarks"
+        return context
+
+
+class BookmarkTagList(BookmarksList):
+    def get_queryset(self):
+        return Bookmark.objects.filter(tags__slug=self.kwargs.get('slug'))
+
+    def get_context_data(self, **kwargs):
+        context = super(BookmarkTagList, self).get_context_data(**kwargs)
+        context['slug'] = self.kwargs.get('slug')
         return context
