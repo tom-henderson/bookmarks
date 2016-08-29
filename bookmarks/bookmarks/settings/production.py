7206f14703d7c18aeeda0977b2f101a155cbfece
Tom Henderson <tomhenderson@mac.com>
2016-08-30 09:21:54 +1200

    Import dj_database_url

--- bookmarks/bookmarks/settings/production.py
+++ bookmarks/bookmarks/settings/production.py
@@ -1,5 +1,6 @@
 """Production settings and globals."""
 from .base import *
+import dj_database_url
 
 # Normally you should not import ANYTHING from Django directly
 # into your settings, but ImproperlyConfigured is an exception.
