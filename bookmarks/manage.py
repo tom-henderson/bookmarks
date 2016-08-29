4d771a30104037c7cef2675a8dd445b670fbeeed
Tom Henderson <tomhenderson@mac.com>
2016-08-30 11:09:14 +1200

    Ghetto logging

--- bookmarks/manage.py
+++ bookmarks/manage.py
@@ -5,6 +5,8 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookmarks.settings.local")
 
+    print "running manage.py"
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
