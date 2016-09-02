a023d304c71721c8c11f6331f2f634ba3b7901ac
Tom Henderson <tomhenderson@mac.com>
2016-09-02 19:50:32 +1200

    Implement search

--- bookmarks/core/views.py
+++ bookmarks/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Q
 
 from .models import Bookmark
 
@@ -13,6 +14,15 @@ class BookmarksList(ListView):
     def get_queryset(self):
         queryset = super(BookmarksList, self).get_queryset()
 
+        search = self.request.GET.get('search', None)
+
+        if search:
+            queryset = queryset.filter(
+                Q(tags__name__contains=search) |
+                Q(title__contains=search) |
+                Q(description__contains=search)
+            ).distinct()
+
         if not self.request.user.is_authenticated():
             queryset = queryset.filter(private=False)
 
@@ -21,6 +31,7 @@ class BookmarksList(ListView):
     def get_context_data(self, **kwargs):
         context = super(BookmarksList, self).get_context_data(**kwargs)
         context['title'] = "Bookmarks"
+        context['search'] = self.request.GET.get('search', None)
         return context
 
 
