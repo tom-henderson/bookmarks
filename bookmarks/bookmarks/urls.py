30ae0997c2ad9e035165c30af13c903f8685aea6
Tom Henderson <tomhenderson@mac.com>
2016-09-17 11:53:18 +1200

    Add basic framework for displaying charts

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 import views
 from core.views import BookmarksList, BookmarkTagList
 from core.views import BookmarkCreate
+from core.views import Charts
 from api.api import API_Bookmarks, API_RecentBookmarks, API_Tags
 import django.contrib.auth.views
 
@@ -16,6 +17,7 @@ urlpatterns = [
     url(r'^$', BookmarksList.as_view(), name='index'),
     url(r'^tag/(?P<slug>[^/]+)/$', BookmarkTagList.as_view(), name='tag'),
     url(r'^new/$', BookmarkCreate.as_view(), name='bookmark_create'),
+    url(r'^charts/$', Charts.as_view(), name='charts'),
     url(r'^api/all/$', API_Bookmarks.as_view(), name='api_all'),
     url(r'^api/recent/$', API_RecentBookmarks.as_view(), name='api_recent'),
     url(r'^api/tags/$', API_Tags.as_view(), name='api_tags'),
